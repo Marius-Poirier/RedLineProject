c16fc6223687a7def22a90181d1b464fb5de64ea
ASAP Marius <marius.poirierpro@gmail.com>
2025-09-29 08:41:17 +0200

    feat: implement reactive form for festival submission with validation

--- src/app/festival-form/festival-form.ts
+++ src/app/festival-form/festival-form.ts
@@ -1,31 +1,36 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FestivalDTO } from '../festival-dto';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-festival-form',
-  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, MatCheckboxModule],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, MatCheckboxModule, ReactiveFormsModule],
   templateUrl: './festival-form.html',
   styleUrl: './festival-form.css'
 })
 export class FestivalForm {
-  form = signal<FestivalDTO>({ id: 0, name: '', location: '', year: 0, isCurrent: false });
   createFestival = output<FestivalDTO>();
 
-  submit(event: Event) {
-    event.preventDefault();
-    const formElement = event.target as HTMLFormElement;
+  readonly form = new FormGroup({
+    name: new FormControl('', { nonNullable: true }),
+    year: new FormControl(2025, { nonNullable: true }),
+    location: new FormControl('', { nonNullable: true }),
+    isCurrent: new FormControl(false, { nonNullable: true })
+    });
+
+  submit(): void {
     const festival: FestivalDTO = {
       id: 0,
-      name: (formElement.elements.namedItem('name') as HTMLInputElement).value,
-      location: (formElement.elements.namedItem('location') as HTMLInputElement).value,
-      year: +(formElement.elements.namedItem('year') as HTMLInputElement).value,
-      isCurrent: (formElement.elements.namedItem('isCurrent') as HTMLInputElement).checked
+      name: this.form.get('name')!.value,
+      location: this.form.get('location')!.value,
+      year: this.form.get('year')!.value,
+      isCurrent: this.form.get('isCurrent')!.value
     };
     this.createFestival.emit(festival);
-    formElement.reset();
+    this.form.reset();
   }
 }
